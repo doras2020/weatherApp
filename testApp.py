e287add178ca4d786d5bec8f4f5f8e9d15ecc76e
doras2020 <76093131+doras2020@users.noreply.github.com>
2021-04-24 18:34:28 +0300

    Add files via upload

--- testApp.py
+++ testApp.py
@@ -8,19 +8,23 @@ class BaseTestCase(TestsValidations):
     def test_current_weather(self):
         weatherP = wp()
         # Validate response.status_code is 200 and return response
-        current_weather = self.valiate_and_parse(
+        current_weather = self.validate_and_parse(
             weatherP.current_weather_request(variablesFile.zipcode, variablesFile.countrycode))
 
-        #Extract the kelvin temperature and turn in to Celsius
-        temp_kelvin = current_weather['main']['temp']
-        temp_celsius = round((float(temp_kelvin) - 273.15), 2)
+        if current_weather['cod'] != '404':
+            #Extract the kelvin temperature and turn in to Celsius
+            temp_kelvin = current_weather['main']['temp']
+            temp_celsius = round((float(temp_kelvin) - 273.15), 2)
 
-        #Verify city and country names are as expected
-        self.assertEqual(current_weather['name'], variablesFile.city)
-        self.assertEqual(current_weather['sys']['country'], variablesFile.countrycode)
+            #Verify city and country names are as expected
+            self.assertEqual(current_weather['name'], variablesFile.city)
+            self.assertEqual(current_weather['sys']['country'], variablesFile.countrycode)
 
-        #Print temperature
-        print(f"The current temperature in {variablesFile.city} is : {str(temp_celsius)} degrees Celsius")
+            #Print temperature
+            print(f"The current temperature in {variablesFile.city} is : {str(temp_celsius)} degrees Celsius")
+
+        else:
+            print(f'Didnt find {variablesFile.city}')
 
 if __name__ == '__main__':
     unittest.main()
